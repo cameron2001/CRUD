7b8b0c794ee47deae0b1ce43cccb94ca2f2edae2
cameron2001 <cameron.purves@hotmail.co.uk>
2015-09-18 15:19:40 +0100

    Hacky fix attempty

--- server.js
+++ server.js
@@ -7,11 +7,8 @@ var bodyParser = require('body-parser');
 var port;
 
 var app = express()
-if (process.argv.indexOf('dev') > -1){
-  port = 8126;
-}else{
-   port = process.env.PORT;
-}
+
+port = process.env.PORT;
 
 app.use('/public', express.static(__dirname + '/public'));
 app.use(bodyParser());
@@ -24,11 +21,8 @@ var server = app.listen(port, function() {
 
 /* Database connection */
 var mongoose = require('mongoose');
-if(port = 8126){
-   mongoose.connect('mongodb://localhost/crudexample');
-}else{
-  mogoose.connect('mongodb://cameron:cameron@ds042898.mongolab.com:42898/cameron-lm');
-}
+mogoose.connect('mongodb://cameron:cameron@ds042898.mongolab.com:42898/cameron-lm');
+
 
 var db = mongoose.connection;
 // Hi Cameron
